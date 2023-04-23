417d8ca063d8d66059973f0c7e48b74f62726c0d
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2023-04-24 02:24:34 +0300

    feat: jwt service provided

--- src/main/java/dev/bl4cktrum/apts/api/models/entities/Relevant.java
+++ src/main/java/dev/bl4cktrum/apts/api/models/entities/Relevant.java
@@ -1,24 +1,70 @@
 package dev.bl4cktrum.apts.api.models.entities;
 
 import dev.bl4cktrum.apts.infrastructure.abstracts.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@Builder
 @Entity
-public class Relevant extends BaseEntity {
+public class Relevant extends BaseEntity implements UserDetails {
+    @Email
+    @NotBlank
     @Column(unique = true)
     private String email;
     private String name;
 
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
     @ManyToMany(mappedBy = "relevants")
     private Set<Patient> patients;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/dev/bl4cktrum/apts/api/repositories/RelevantRepository.java
+++ src/main/java/dev/bl4cktrum/apts/api/repositories/RelevantRepository.java
@@ -1,11 +1,10 @@
 package dev.bl4cktrum.apts.api.repositories;
 
-import dev.bl4cktrum.apts.api.models.entities.Patient;
 import dev.bl4cktrum.apts.api.models.entities.Relevant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface RelevantRepository extends JpaRepository<Relevant, String> {
+    Optional<Relevant> findByEmail(String email);
 }
