c6576d484ca4762ec5bb20e906e86f77e14530ad
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2023-04-30 00:12:44 +0300

    fix: 403 response status on every exception has resolved with adding '/error' to security's whitelist

---
+++ src/main/java/dev/bl4cktrum/apts/api/exceptions/ApiException.java
@@ -0,0 +1,11 @@
+package dev.bl4cktrum.apts.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+public class ApiException extends RuntimeException{
+    public ApiException(String message) {
+        super(message);
+    }
+}
