21f1ce11dfe3df0e95cea4ffb4db6f3d5c968380
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2023-04-30 00:47:46 +0300

    feat: built a test endpoint for postgis types

--- src/main/java/dev/bl4cktrum/apts/api/controllers/TestController.java
+++ src/main/java/dev/bl4cktrum/apts/api/controllers/TestController.java
@@ -1,8 +1,13 @@
 package dev.bl4cktrum.apts.api.controllers;
 
+import dev.bl4cktrum.apts.api.exceptions.ApiException;
+import dev.bl4cktrum.apts.api.models.requests.LoginRequest;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import net.postgis.jdbc.geometry.Polygon;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/api/v1/test")
@@ -18,5 +23,17 @@ public class TestController {
     @GetMapping("/auth-not-needed")
     public ResponseEntity<String> testTwo(){
         return ResponseEntity.ok("hello not authenticated world.");
+    }
+    @PostMapping("/polygon-create")
+    public ResponseEntity<String> polygonCreate(@RequestBody LoginRequest request){
+        throw new ApiException("test");
+//        try {
+//            Polygon p1 = new Polygon("(50.6373 3.0750,50.6374 3.0750,50.6374 3.0749,50.63 3.07491,50.6373 3.0750)");
+//            System.out.println(p1.getRing(0));
+//            return ResponseEntity.ok(p1.toString());
+//        } catch (SQLException e) {
+//            System.err.println(e);
+//            return null;
+//        }
     }
 }
