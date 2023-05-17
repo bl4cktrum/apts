0d3704c5aacc279ed03320b42aec1140788b4ea8
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2023-05-17 11:05:29 +0300

    refactor: patientIDin MatchRequestModel validate with custom pattern to match with uuid returned watchos

--- src/main/java/dev/bl4cktrum/apts/api/models/requests/MatchRequest.java
+++ src/main/java/dev/bl4cktrum/apts/api/models/requests/MatchRequest.java
@@ -1,13 +1,13 @@
 package dev.bl4cktrum.apts.api.models.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import org.hibernate.validator.constraints.UUID;
 
 @Data
 public class MatchRequest {
     @NotBlank()
-    @UUID
+    @Pattern(regexp="^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$")
     private String patientDeviceId;
 
     @NotBlank()
